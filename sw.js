43bf1140b4b3ad119e483e61262e622f52c68b58
trevor-web-cloud <tsai0215@gmail.com>
2025-08-11 21:20:27 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Math Quiz PWA Service Worker (cache-first)
-const CACHE_VERSION = 'v1.3.3-unified';
+const CACHE_VERSION = 'v1.3.4-unified2';
 const CACHE_NAME = `math-quiz-cache-${CACHE_VERSION}`;
 const ASSETS = [
   './',
